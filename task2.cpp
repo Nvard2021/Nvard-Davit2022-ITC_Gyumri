0c881ecf0a2bb6d9d820783ff27bb483dd323fe4
Nvard <nvard.badalyan.im@gmail.com>
2022-01-13 16:06:02 +0400

    Task2

---
+++ task2.cpp
@@ -0,0 +1,19 @@
+// Sort the arrey
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int arr[] = { 1, 8, 6 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	sort(arr, arr + n);
+
+	cout << "\nSort: ";
+	for (int i = 0; i < n; ++i)
+		cout << arr[i] << " " ;
+
+	return 0;
+}
+
