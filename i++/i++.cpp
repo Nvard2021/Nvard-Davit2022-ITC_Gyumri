4f427580e8a90bf83869a9aec8ff973eebe06f54
Davit2022 <davit.panosyan.im@gmail.com>
2022-01-17 21:50:03 +0400

    Added file

---
+++ i++/i++.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int number;
+    int number1;
+    cout << "Enter number" << endl;
+    cin >> number;
+    number1 = number;
+    cout << "Number++ = " << number++ << endl;
+    cout << "++Number = " << ++number1 << endl;
+}
