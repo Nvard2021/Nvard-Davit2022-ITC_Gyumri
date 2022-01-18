cf282354fe8572b39c744d961e3d9662ba35e826
Davit2022 <davit.panosyan.im@gmail.com>
2022-01-18 15:41:02 +0400

    Changed file

--- i++/i++.cpp
+++ i++/i++.cpp
@@ -6,6 +6,10 @@ int main(){
     cout << "Enter number" << endl;
     cin >> number;
     number1 = number;
+    int number2 = number;
+    int number3 = number;
     cout << "Number++ = " << number++ << endl;
     cout << "++Number = " << ++number1 << endl;
+    cout << "***********\nNumber-- = " << number2-- << endl;
+    cout << "--Number = " << --number3 << endl;
 }
