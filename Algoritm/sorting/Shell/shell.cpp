f1ffcaf8645630c434ec21d407a80c7cfcb714e9
Davit2022 <davit.panosyan.im@gmail.com>
2022-01-27 01:46:12 +0400

    Added file

---
+++ Algoritm/sorting/Shell/shell.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+void input_elements(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << "Enter namber for "
+			 << "index " << i << " : ";
+		cin >> arr[i];
+	}
+}
+void printArray(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << "index " << i << " : " << arr[i] << endl;
+	}
+}
+int shellsort(int arr[], int size)
+{
+	for (int interval = size / 2; interval > 0; interval /= 2)
+	{
+		for (int i = interval; i < size; i++)
+		{
+			int temp = arr[i];
+			int gap;
+			for (gap = i; gap >= interval && arr[gap - interval] > temp; gap -= interval)
+			{
+				arr[gap] = arr[gap - interval];
+			}
+			arr[gap] = temp;
+		}
+	}
+	return 0;
+}
+int main()
+{
+	int arr[5];
+	int size = sizeof(arr) / sizeof(int);
+	input_elements(arr, size);
+	printArray(arr, size);
+	cout << "Shell Sorting" << endl;
+	shellsort(arr, size);
+	printArray(arr, size);
+}
