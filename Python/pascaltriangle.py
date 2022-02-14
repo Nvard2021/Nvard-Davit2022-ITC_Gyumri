97c419f009b1ad438a3060af52cfed2ddf18fff8
Nvard2021 <95294290+Nvard2021@users.noreply.github.com>
2022-02-14 23:06:32 +0400

    Update pascaltriangle.py

--- Python/pascaltriangle.py
+++ Python/pascaltriangle.py
@@ -1,9 +1,31 @@
-def pascal_triangle(n):
-   trow = [1]
-   y = [0]
-   for x in range(max(n,0)):
-      print(trow)
-      trow=[l+r for l,r in zip(trow+y, y+trow)]
-   return n>=1
-pascal_triangle(6) 
+rown = 8
+dp = []
+for i in range(rown):
+    cur =  []
+    for j in range(i+1):
+        cur.append(0)
+    if i == 0:
+        cur = [1]
+    else:
+         cur[0] = 1 
+         cur[len(cur) -1] =1 
+    dp.append(cur)
+print(dp)
 
+for i in range(2,rown):
+    for j in range(1,i):
+        dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+
+
+print(dp)
+
+
+
+# 1
+# 11
+# 121
+# 1331
+
+# dp[2][1] = dp[1][0] + dp[i][1];
+# dp[i][j] = dp[i-1][j-1] + dp[i-1][j]
+# 
